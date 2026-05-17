8988798db5ee99d84bca7443b00e59a68ce61b39
buffbot88 <buffbot88@gmail.com>
2026-05-17 00:49:55 -0400

    Delete frontend directory

--- frontend/src/core/platform/index.js
+++
@@ -1,3 +0,0 @@
-export { PlatformCoreProvider, usePlatformCore, usePlatformAuditLog, usePlatformReplay, replayActions } from './PlatformCoreContext'
-export { CORE_ACTION_TYPES, createPlatformAction, ACTION_EXAMPLES } from './actions'
-export { createPluginRuntime, sampleLoggerPlugin } from './PluginRuntime'
--- frontend/src/modules/site-builder/renderers/index.js
+++
@@ -1,3 +0,0 @@
-export { SiteRenderer } from './SiteRenderer'
-export { SinglePageRenderer } from './SinglePageRenderer'
-export { MultiPageRenderer } from './MultiPageRenderer'
